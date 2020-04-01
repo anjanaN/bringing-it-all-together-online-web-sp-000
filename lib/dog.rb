539824f51d3b87b48dcb709736e8a6394a65e29d
Anjana Nair <anju740@gmail.com>
2020-04-01 19:14:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,8 +17,12 @@ class Dog
   end
 
   def save
+    if self.id
+      self.update
+    else
     DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+  end
     self
   end
 
